7558b00db8ef060ffe5188756d900a8fa0324b2b
David Garber <dgarber@microsoft.com>
2023-08-01 13:02:06 -0700

    Moon

--- 23/puzzle_list.js
+++ 23/puzzle_list.js
@@ -29,7 +29,7 @@ var puzzles = [
     { title: 'Weird, You Nits', author: 'Martyn Lovell', type: types.rebus, group: group.portrait },
     { title: 'Buried Logic', author: 'Martyn Lovell', type: types.word, group: group.portrait },
     { title: 'Fish Story', author: 'Dana Young', type: types.rebus, group: group.portrait },
-    { title: 'Moon', author: 'Rorke Haining', type: types.word, group: group.pending },
+    { title: 'Moon', author: 'Rorke Haining', type: types.word, group: group.portrait },
     { title: 'Petri Dish', author: 'Peter Golde', type: types.word, group: group.pending },
     { title: 'Complex Molecules', author: 'Philippe ', type: types.word, group: group.pending },
     { title: 'Engineering', author: 'Dana Young', type: types.logic, group: group.pending },
