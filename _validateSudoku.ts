30954a624695583f178731328983ffc7a8dda573
davidggarber <davidggarber@hotmail.com>
2024-03-21 01:08:12 -0700

    Andrew's Reversi

---
+++ _validateSudoku.ts
@@ -0,0 +1,11 @@
+
+type sudokuErrors = 
+{
+  columns: Array<boolean|undefined>,
+  rows: Array<boolean|undefined>,
+  squares: Array<boolean|undefined>,
+};
+
+function validateSudoku(grid:object[][]): sudokuErrors {
+
+}
