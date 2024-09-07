073d8e622bddb5132282555ead8eea39bfad82c7
davidggarber <davidggarber@hotmail.com>
2024-09-06 22:52:17 -0700

    Fridge Raider #3 as a meta

--- 24/V/sgb.js
+++ 24/V/sgb.js
@@ -1,1 +1,3 @@
-validation={"final-answer":{"FJRQVFUPURS":"1","GURFJRQVFUPURS":"1","FNLFOBEXOBEXOBEX":"3Xrrc tbvat! Jub fnlf guvf?"}};
+// validation={"final-answer":{"FJRQVFUPURS":"1","GURFJRQVFUPURS":"1","FNLFOBEXOBEXOBEX":"3Xrrc tbvat! Jub fnlf guvf?"}};
+validation={"final-answer":{"FJRQVFUPURS":"1","GURFJRQVFUPURS":"1","FNLFOBEXOBEXOBEX":"3Xrrc tbvat! Jub fnlf guvf?","PYHR":"5FO_Tvbegv.kugzy|5FO_Wnapuv.kugzy|5FO_Bghehagmn.kugzy|5FO_Irvgfyn.kugzy"}};
+
