1bc02e489c43a276949ebb33a70bf2a5a70a3be2
davidggarber <davidggarber@hotmail.com>
2024-12-28 04:12:42 -0800

    Rebus

---
+++ DnancXmas/V/rbus.js
@@ -0,0 +1,1 @@
+validation={"":{"UHAF":"1Jura thrfgf frrx n xvpxre naq dhvpxre,\nSebz gubfr ebjf bs obggyrf tebja guvpxre,\nSvaq ovggref naq zvkref, \nNaq urnql ryvkvef, \nGurl novqr va gur pnovarg bs ______."}};
