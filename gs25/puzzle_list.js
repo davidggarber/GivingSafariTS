575198ca96689c60d49f7abf17a318ed2125cd16
David Garber <dgarber@microsoft.com>
2025-07-12 23:55:48 -0700

    Cartography (partial) Disney

--- gs25/puzzle_list.js
+++ gs25/puzzle_list.js
@@ -33,7 +33,7 @@ var puzzles = [
     { title: 'America the Beautiful', thumb: 'tbd', author: 'Chris Jeuell', type: types.word, group: group.puzzle, orientation: orient.portrait, ir:false, feeder: [] },
     { title: 'Biomes of the World', thumb: 'tbd', author: 'Zhenya Ross', type: types.word, group: group.puzzle, orientation: orient.portrait, ir:false, feeder: [] },
     { title: 'Cartography', thumb: 'tbd', author: 'Jake Lui', type: types.word, group: group.puzzle, orientation: orient.portrait, ir:false, feeder: [] },
-    // { title: 'Disney and the Great Outdoors', thumb: 'tbd', author: 'Chris Jeuell', type: types.word, group: group.puzzle, orientation: orient.portrait, ir:false, feeder: [] },
+    { title: 'Disney and the Great Outdoors', thumb: 'tbd', author: 'Chris Jeuell', type: types.word, group: group.puzzle, orientation: orient.portrait, ir:false, feeder: [] },
     // { title: 'Down Under', thumb: 'tbd', author: 'Wei-Hwa Huang', type: types.word, group: group.puzzle, orientation: orient.portrait, ir:false, feeder: [] },
     // { title: 'Down, Across, and All Around', thumb: 'tbd', author: 'Wei-Hwa Huang', type: types.word, group: group.puzzle, orientation: orient.portrait, ir:false, feeder: [] },
     // { title: 'Fences', thumb: '', author: 'Martyn Lovell', type: types.word, group: group.puzzle, orientation: orient.portrait, ir:false, feeder: [] },
