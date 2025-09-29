ed4c61ee666ac4c0d3446974cde68963bfbcd6cb
davidggarber <davidggarber@hotmail.com>
2025-09-29 00:38:12 -0700

    Add hint links Add benevity links

--- Posters/QRs/make.bat
+++ Posters/QRs/make.bat
@@ -1,4 +1,5 @@
 D:\vsts\QRCode\Console\bin\Release\net6.0-windows\QRMake.exe -format="svg.circle" -save="givingps.svg" https://aka.ms/givingps
+D:\vsts\QRCode\Console\bin\Release\net6.0-windows\QRMake.exe -format="svg.circle" -save="benevity.svg" https://microsoft.benevity.org/cause/840-912045918
 D:\vsts\QRCode\Console\bin\Release\net6.0-windows\QRMake.exe -format="svg.circle" -save="gs25.svg" https://www.puzzyle.net/gs25/Map25.xhtml
 D:\vsts\QRCode\Console\bin\Release\net6.0-windows\QRMake.exe -format="svg.circle" -save="Gumball.svg" https://www.puzzyle.net/Posters/Gumball.xhtml?gs25
 D:\vsts\QRCode\Console\bin\Release\net6.0-windows\QRMake.exe -format="svg.circle" -save="SpaghettiCode.svg" https://www.puzzyle.net/Posters/SpaghettiCode.xhtml?gs25
--- Posters/poster.js
+++ Posters/poster.js
@@ -10,8 +10,8 @@ function posterTemplates() {
           <div style="margin-left: 2.5in">
             <h1 style="margin-right: 1.25in">Giving Puzzle Safari</h1>  
             <h2>Enjoy puzzles? Words / Logic / Math/ Trivia / More!</h2>
-            <h2>Join us at <a href="https://www.puzzyl.net/gs25/Map25.xhtml">aka.ms/givingps</a>.
-              Donations benefit <a href="aka.ms/benevity">Rainier Scholars</a>.
+            <h2>Join us at <a href="https://aka.ms/givingps" target="_blank">aka.ms/givingps</a>.
+              Donations benefit <a href="https://microsoft.benevity.org/cause/840-912045918" target="_blank">Rainier Scholars</a>.
               <br />Puzzles released Fridays, October 3<sup>rd</sup> / 10<sup>th</sup> / 17<sup>th</sup> / 24<sup>th</sup>.</h2>
           </div>
         </div>`;
@@ -24,9 +24,11 @@ function posterTemplates() {
           <img class="banner" src="../gs25/Images/GS25_banner.png" />
           <div style="margin-right: 2.5in">
             <h1>Think you have a solution?</h1>
-            <h2>Submit your guess online: <img class="solve-qr" src="QRs/{solveqr}.svg" />
-            </h2>
+            <h2>Submit your guess online: <img class="solve-qr" src="QRs/{solveqr}.svg" /></h2>
           </div>
+        </div>
+        <div id="hint-link">
+          <h3>Need a hint? Email <a href="mailto:givingps@microsoft.com">GivingPS&#x2009;@&#x2009;microsoft.com</a>.</h3>
         </div>`;
   body.appendChild(temp);
 }
